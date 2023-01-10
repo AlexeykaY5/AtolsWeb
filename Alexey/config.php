38dd156f870121b3177a072d8b1e7c3deb099834
unknown <alexeysheggi@mail.ru>
2023-01-10 03:49:01 +0100

    init

---
+++ Alexey/config.php
@@ -0,0 +1,36 @@
+<?php
+// APPLICATION
+define('APPLICATION', 'Admin');
+
+// HTTP
+define('HTTP_SERVER', 'http://opentest/Alexey/');
+define('HTTP_CATALOG', 'http://opentest/');
+
+// DIR
+define('DIR_OPENCART', 'C:/OSPanel/domains/opentest/');
+define('DIR_APPLICATION', DIR_OPENCART . 'Alexey/');
+define('DIR_EXTENSION', DIR_OPENCART . 'extension/');
+define('DIR_IMAGE', DIR_OPENCART . 'image/');
+define('DIR_SYSTEM', DIR_OPENCART . 'system/');
+define('DIR_CATALOG', DIR_OPENCART . 'catalog/');
+define('DIR_STORAGE', 'C:/OSPanel/domains/storage/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'TestAdmin');
+define('DB_PASSWORD', 'TestAdmin1221');
+define('DB_DATABASE', 'test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
